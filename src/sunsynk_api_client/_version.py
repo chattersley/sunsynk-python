8796b3609b26e3e2ae5dfb5a84aa8d490d0b3ca1
Craig Hattersley <craig@hattersley.io>
2026-03-14 10:49:58 +0000

    Moved api spec inside project

--- src/sunsynk_api_client/_version.py
+++ src/sunsynk_api_client/_version.py
@@ -4,10 +4,10 @@
 import importlib.metadata
 
 __title__: str = "sunsynk-api-client"
-__version__: str = "0.1.6"
+__version__: str = "0.1.7"
 __openapi_doc_version__: str = "1.1.0"
-__gen_version__: str = "2.862.0"
-__user_agent__: str = "speakeasy-sdk/python 0.1.6 2.862.0 1.1.0 sunsynk-api-client"
+__gen_version__: str = "2.865.2"
+__user_agent__: str = "speakeasy-sdk/python 0.1.7 2.865.2 1.1.0 sunsynk-api-client"
 
 try:
     if __package__ is not None:
